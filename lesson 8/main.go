2d0a9c6c0955f56b44f0a0ec6de3455ce19b67ff
koiEugene <e.bovtrukevich@gmail.com>
2022-10-11 01:52:06 +0300

    First commit

--- lesson 8/main.go
+++ lesson 8/main.go
@@ -1,8 +1,13 @@
 package main
 
+import "fmt"
+
 func abc(x, y int) int {
 	return x + y
 }
 func main() {
-
+	f := abc(2, 3)
+	fmt.Println("resalt = ", f)
+	a := abc(3, 4)
+	fmt.Println("result a =", a)
 }
